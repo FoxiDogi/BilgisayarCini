e18ff6edd72337c6d4e9989b0d07fff65ada1717
FoxiDogi <hmtarhan@gmail.com>
2018-03-28 23:14:11 +0300

    Add project files.

---
+++ BilgisayarCini/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace BilgisayarCini
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.QuestionButton = new System.Windows.Forms.Button();
+            this.AnswerLbl = new System.Windows.Forms.Label();
+            this.QuestionBox = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // QuestionButton
+            // 
+            this.QuestionButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            resources.ApplyResources(this.QuestionButton, "QuestionButton");
+            this.QuestionButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.QuestionButton.Name = "QuestionButton";
+            this.QuestionButton.UseVisualStyleBackColor = false;
+            this.QuestionButton.Click += new System.EventHandler(this.QuestionButton_Click);
+            // 
+            // AnswerLbl
+            // 
+            resources.ApplyResources(this.AnswerLbl, "AnswerLbl");
+            this.AnswerLbl.Name = "AnswerLbl";
+            // 
+            // QuestionBox
+            // 
+            resources.ApplyResources(this.QuestionBox, "QuestionBox");
+            this.QuestionBox.Name = "QuestionBox";
+            this.QuestionBox.TextChanged += new System.EventHandler(this.QuestionBox_TextChanged);
+            this.QuestionBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.QuestionBox_KeyDown);
+            this.QuestionBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.QuestionBox_KeyPress);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.QuestionButton;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.QuestionBox);
+            this.Controls.Add(this.AnswerLbl);
+            this.Controls.Add(this.QuestionButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button QuestionButton;
+        private System.Windows.Forms.Label AnswerLbl;
+        private System.Windows.Forms.TextBox QuestionBox;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BilgisayarCini/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BilgisayarCini
+{
+    public partial class Form1 : Form
+    {
+        bool pressed = true;
+        string question = "Lütfen sorumu cevapla bilgisayar cini sana yalvarıyorum";
+        char[] qustionChar;        
+        int index = 0;
+        string myText = "";
+        string answer;
+        string[] errorMessages;
+        public Form1()
+        {
+            InitializeComponent();
+            qustionChar = question.ToCharArray();
+            errorMessages = new string[] {"Git Başımdan","Rahat Bırak","Git","Anladın mı?","GİİİİTTT!!!!" };
+        }
+        public bool Security()
+        {
+            if (answer==null)
+                return false;
+
+            return true;
+        }
+        private void QuestionBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!pressed)
+            {
+                QuestionBox.Text = myText;
+                QuestionBox.SelectionStart = QuestionBox.Text.Length;
+            }
+        }
+
+        private void QuestionBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey)
+                pressed = !pressed;
+        }
+
+        private void QuestionBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!pressed)
+            {
+                answer += e.KeyChar.ToString();
+                myText = QuestionBox.Text + qustionChar[index];
+                index++;
+            }
+        }
+
+        private void QuestionButton_Click(object sender, EventArgs e)
+        {
+            Random waitSeconds = new Random();
+            int second = waitSeconds.Next(3, 10);
+            timer1.Interval = second * 1000;
+            timer1.Enabled = true;
+
+            Cursor = Cursors.WaitCursor;
+            QuestionButton.Enabled = false;
+            QuestionBox.Enabled = false;
+            ControlBox = false;
+            QuestionButton.Text = "Düşünüyorum";
+            Text = "Bilgisayar Cini(Düşünüyor)";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+
+            QuestionBox.Text = "";
+            if (Security())
+            {
+                DialogResult result = MessageBox.Show(answer, "Cevabım", MessageBoxButtons.OK, MessageBoxIcon.Information,MessageBoxDefaultButton.Button1);
+                if (result == DialogResult.OK)
+                {
+                    Reset();
+                }
+            }
+            else
+            {
+              DialogResult result = MessageBox.Show("SEN KİMSİN!!!","GİT BAŞIMDAN",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                if (result == DialogResult.OK)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        MessageBox.Show(errorMessages[i],errorMessages[i],MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    }
+                    Application.Exit();
+                }
+            }
+        }
+
+        void Reset()
+        {
+            
+            answer = null;
+            myText = "";
+            pressed = true;
+            index = 0;
+            QuestionButton.Enabled = true;
+            QuestionBox.Enabled = true;
+            Cursor = Cursors.Default;
+            ControlBox = true;
+            Text = "Bilgisayar Cini";
+            QuestionBox.Focus();
+            QuestionButton.Text = "Sor";
+        }
+    }
+}
